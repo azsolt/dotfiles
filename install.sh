b6631dd751782e279ff86a44256704700dbd1d6f
Azsolt Kasa <azsoltkasa@gmail.com>
2013-10-06 01:26:14 -0400

    whitespace fix

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 function do_install() {
-	# install function to be added
+    # install function to be added
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	do_install
+    do_install
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		do_install
-	fi
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        do_install
+    fi
 fi
+
 unset do_install
