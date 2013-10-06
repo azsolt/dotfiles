1a145aa3657e0081d2182e76bf09749cd45b2dda
Azsolt Kasa <azsoltkasa@gmail.com>
2013-10-06 01:11:33 -0400

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function do_install() {
+	# install function to be added
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	do_install
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		do_install
+	fi
+fi
+unset do_install
