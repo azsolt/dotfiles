5e7e3f7a81596ca4856a0fce10f5095e40541217
Azsolt Kasa <azsoltkasa@gmail.com>
2013-10-10 15:56:14 -0400

    changed bootstrap script to not allow root install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-user=$SUDO_USER
-
 die() {
     echo $1
     exit
 }
 
+if [ $USER == root ]; then
+    die "bootstrap.sh: do not run as root (sudo)"
+fi
+
 oh_my_zsh() {
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | bash
 }
-
-if [ $USER != root ]; then
-    die "bootstrap.sh must run as root"
-fi
 
 install_apps="
     git
@@ -24,16 +22,16 @@ install_apps="
 for app in $install_apps
 do 
     echo "Installing $app..."
-    apt-get install -y $app
+    sudo apt-get install -y $app
 done
 
-apt-get autoremove -y
+sudo apt-get autoremove -y
 oh_my_zsh
 
 # clone azsolt/dotfiles and run install.sh
 if which git > /dev/null; then
     if [ ! -d ~/.dotfiles ]; then
-        sudo -u $user git clone https://github.com/azsolt/dotfiles.git ~/.dotfiles
+        git clone https://github.com/azsolt/dotfiles.git ~/.dotfiles
 
         if [ -d ~/.dotfiles ]; then
             source ~/.dotfiles/install.sh
