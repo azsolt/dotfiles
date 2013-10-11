d835d46db331ff62b0d467d314bfcbf95c0df45b
Azsolt Kasa <azsoltkasa@gmail.com>
2013-10-11 16:30:20 -0400

    update install script

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
+# folders
+dropbox_dir=~/Dropbox
+# =====================================================================================
+
+# functions
 die() {
     echo $1
     exit
 }
+# =====================================================================================
 
 if [ $USER == root ]; then
     die "bootstrap.sh: do not run as root (sudo)"
 fi
+
+cd ~
 
 # installing environment apps
 
@@ -21,57 +29,63 @@ install_apps="
 echo "Installing apps $install_apps"
 sudo apt-get install -y $install_apps
 echo "==="
+# =====================================================================================
 
 # cleaning
 echo "Cleaning..."
 sudo apt-get autoremove -y
 echo "==="
+# =====================================================================================
+
+# install oh-my-zsh, zsh (shell)
+if ! which zsh > /dev/null; then
+    echo "Installing oh-my-zsh..."
+    curl -sL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | bash
+    echo "==="
+fi
+# =====================================================================================
 
 # install pip packages
 if ! which pip > /dev/null; then
     die "Pip not installed"
 fi
+# =====================================================================================
 
-echo "Installing oh-my-zsh..."
-# install oh-my-zsh, zsh (shell)
-curl -sL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | bash
-echo "==="
-
-# clone azsolt/dotfiles
-if which git > /dev/null; then
-    if [ ! -d ~/.dotfiles ]; then
-        echo "Cloning git repository..."
-        git clone https://github.com/azsolt/dotfiles.git ~/.dotfiles
-        echo "==="
-    fi
-else
-    die "Git was not installed, </3 check for errors and try again"
-fi
-
-# install dotfiles
-if [ -d ~/.dotfiles ]; then
-    # copy all files from config to ~/.config
-    echo "Copying configuration files..."
-    rsync -av ~/.dotfiles/config/* ~/.config
+# if this was curl install, we must clone our repo
+if [ ! -d ~/.dotfiles ]; then
+    echo "Cloning git repository..."
+    git clone git@github.com:azsolt/dotfiles.git ~/.dotfiles
     echo "==="
-
-    # link every .symlink file into ~
-    for source in `find ~/.dotfiles -maxdepth 2 -name \*.symlink.sh`
-    do
-        dest="$HOME/.`basename $source .symlink.sh`"
-
-        if [ -f $dest ]; then
-            mv $dest $dest\.backup
-            echo "Backing up your current $dest to $dest.backup"
-        fi
-
-        rm -rf $dest
-        ln -s $source $dest
-        echo "Linked $dest to $source"
-        echo "==="
-    done
-
-    echo "Dotfiles installed :)"
-else
-    die "Failed to clone azsolt/dotfiles repository"
 fi
+# =====================================================================================
+
+# dropbox
+echo "Installing Dropbox..."
+    ~/.dotfiles/bin/dropbox start -i
+echo "==="
+# =====================================================================================
+
+# copy all files from ~/.dotfiles/config to ~/.config
+echo "Copying configuration files..."
+rsync -av ~/.dotfiles/config/* ~/.config
+echo "==="
+# =====================================================================================
+
+# link every .symlink file into ~
+for source in `find $DOT -maxdepth 2 -name \*.symlink.sh`
+do
+    dest="$HOME/.`basename $source .symlink.sh`"
+
+    if [ -f $dest ]; then
+        mv $dest $dest\.backup
+        echo "Backing up your current $dest to $dest.backup"
+    fi
+
+    rm -rf $dest
+    ln -s $source $dest
+    echo "Linked $dest to $source"
+    echo "==="
+done
+# =====================================================================================
+
+echo "Dotfiles installed :)"
