1a8eb9de3daaabae2583012b9019afe2e663b061
Azsolt Kasa <azsoltkasa@gmail.com>
2013-10-17 21:09:30 -0300

    added some git aliases

---
+++ git/aliases.source.sh
@@ -0,0 +1,8 @@
+# https://github.com/mathiasbynens/dotfiles/blob/master/.aliases
+
+# Custom Git Aliases - more in oh-my-zsh plugins
+
+# git list untracked files
+alias glsun="git ls-files -o --exclude-standard"
+# fint .gitignore files
+alias gfig="find -name \.gitignore"
